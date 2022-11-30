004caf7908bae38e791dc80858e6a6f60e72bc81
phisitphong-kong <phisitphong.k@ku.th>
2022-11-30 11:49:43 +0700

    fix data

--- ad_status_edit.php
+++ ad_status_edit.php
@@ -17,16 +17,16 @@ $add_id = $_GET['ROOM_ID'];
 $status = 'ว่าง';
 //sql update
 $stmt = $conn->prepare("UPDATE  admin_add SET ROOM_STATUS = '$status'  WHERE ROOM_ID='$add_id'");
-
-
 $stmt->execute();
+$do = $conn->prepare("DELETE FROM reserve WHERE room_id=$add_id");
+$do->execute();
 //  sweet alert 
 echo '
     <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert-dev.js"></script>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.css">';
 
-  if($stmt->rowCount() ==1){
+  if($stmt->rowCount() ==1 && $do->rowCount() ==1){
         echo '<script>
              setTimeout(function() {
               swal({
